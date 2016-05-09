5baf8f9788add2331426c64f1b9e3b8f7981bc89
Märt Bakhoff <mart.bakhoff@gmail.com>
2016-05-09 12:32:51 +0300

    typo

--- src/main/java/bitwise/Color.java
+++ src/main/java/bitwise/Color.java
@@ -3,7 +3,7 @@ package bitwise;
 /**
  * Holds a color value with 8-bit red, green, blue and alpha components.
  * The components are packed into a single 32-bit integer, where the
- * component order from right to left is: red, green, blue, alpha.
+ * component order from left to right is: red, green, blue, alpha.
  */
 public class Color {
 
