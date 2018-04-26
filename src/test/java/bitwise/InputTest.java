6956230e2fdf04fc4584b6c876afdd2a834e54d4
Märt Bakhoff <mart.bakhoff@gmail.com>
2018-04-26 08:55:55 +0300

    add the testTwoUTFNonEmpty test

--- src/test/java/bitwise/InputTest.java
+++ src/test/java/bitwise/InputTest.java
@@ -28,9 +28,20 @@ public class InputTest {
     InputStream stream = TestUtil.prepare(out -> {
       out.writeInt(7);
       out.write(new byte[]{97, 49, -61, -74, -30, -126, -84});
-      out.write(new byte[]{'w', 't', 'f'}); // trailing garbage
     });
     assertEquals("a1ö€", Input.readUTF(stream));
+  }
+
+  @Test
+  public void testTwoUTFNonEmpty() throws Exception {
+    InputStream stream = TestUtil.prepare(out -> {
+      out.writeInt(3);
+      out.write(new byte[]{'a', 'b', 'c'});
+      out.writeInt(3);
+      out.write(new byte[]{'x', 'y', 'z'});
+    });
+    assertEquals("abc", Input.readUTF(stream));
+    assertEquals("xyz", Input.readUTF(stream));
   }
 
   @Test
